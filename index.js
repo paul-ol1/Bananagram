a789c54a9c575bb674d757233420bb6dc2284324
paul-ol1 <oladelepaul3@gmail.com>
2023-05-08 15:43:22 +0200

    hopefully before last update

--- index.js
+++ index.js
@@ -100,7 +100,6 @@ app.post("/sharetiles", function (req, res) {
     });
     await getalltiles(gameid).then((y) => {
       alltiles = y[0].GameTiles.split("");
-      console.log("alltiles: "+ alltiles.length);
       for (let x = 0; x < allusers.length; x++) {
         if (allusers.length >= 1 && allusers.length <= 4){
           currtiles = alltiles.splice(0, 21);
@@ -117,7 +116,6 @@ app.post("/sharetiles", function (req, res) {
           ` UPDATE Players SET PlayerTiles= '${currtiles}' WHERE PlayerID= ${allusers[x].PlayerID}`
         );
       }
-      console.log("returning length:"+ alltiles.length);
       alltiles = alltiles.join("");
       returntiles(alltiles, gameid);
       res.send(currtiles);
@@ -337,7 +335,6 @@ app.post("/createng", function (req, res) {
   async function onetime() {
     if (req.cookies.Playerdetails === undefined) {
       let tiles = createtiles();
-      console.log(tiles.length);
       let x = await createnewgame(tiles, gn);
       let y = await createnewplayer(x[0].GameID, pname);
 
